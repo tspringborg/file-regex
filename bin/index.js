a5260ca597b0c735de0c7c123964b0039ff4779f
Thomas Springborg <ts@molamil.com>
2015-03-06 16:38:13 +0100

    more cli like structure

--- bin/index.js
+++ bin/index.js
@@ -1,33 +1,47 @@
+#!/usr/bin/env node
+
 /**
  * Created by ts on 06/03/15.
  */
 var fs = require('fs')
 var walk = require('walkdir');
-var _ = require('underscore');
 var path = require('path');
-
 var type = process.argv[2];
 var fileNameMatchRegex = new RegExp(process.argv[3]);
 var fileDataRegex = new RegExp(process.argv[4], "g");
 var fileDataReplace = process.argv[5];
 console.log('file-regex in directory: ' + process.cwd());
+console.log('type: ' + type);
 console.log('fileNameMatchRegex: '+fileNameMatchRegex);
 console.log('fileDataRegex: '+fileDataRegex);
 console.log('fileDataReplace: '+fileDataReplace);
-walk(process.cwd(),function(path,stat){
-    //if not dir
-    if(path.match(/\.(.){2,}/gi)){
-        //console.log(path+' is file');
-        var filename = path.split('/');
-        filename = filename[filename.length-1];
-        if(filename.match(fileNameMatchRegex)){
-            //console.log("file-match: "+path);
-            replaceInFile(path);
+switch(type){
+    case 'help':
+        break;
+    case 'replace':
+    case 'test':
+        initWalker();
+}
+
+function initWalker(){
+    walk(process.cwd(),function(path,stat){
+        //if not dir
+        if(path.match(/\.(.){2,}/gi)){
+            //console.log(path+' is file');
+            var filename = path.split('/');
+            filename = filename[filename.length-1];
+            if(filename.match(fileNameMatchRegex)){
+                if(type == 'replace'){
+                    replaceInFile(path);
+                }else if(type == 'test'){
+                    console.log("file-match: "+path);
+                }
+            }
+        }else{
+            //console.log(path+' is not file');
         }
-    }else{
-        //console.log(path+' is not file');
-    }
-});
+    });
+}
 
 function replaceInFile(file){
     console.log('replaceInFile: '+file);
@@ -41,11 +55,6 @@ function replaceInFile(file){
             if (err) return console.log(err);
             console.log('replace done: '+file);
         });
-        //var result = data.replace(/string to be replaced/g, 'replacement');
-        //
-        //fs.writeFile(someFile, result, 'utf8', function (err) {
-        //    if (err) return console.log(err);
-        //});
     });
 
 }
